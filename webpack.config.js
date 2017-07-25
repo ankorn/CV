9b03e2573cdc9cba68f8e281290c66b61912c841
Anton Korneyev <an.korn.zasekin@gmail.com>
2017-07-25 19:44:58 +0400

    add airbnb eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
-var path = require('path')
+const path = require('path');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: [
-        './src/index.js'
+  devtool: 'source-map',
+  entry: [
+    './src/index.jsx',
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.css/,
+        loaders: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js/,
-                loaders: ['babel-loader'],
-                include: path.join(__dirname, 'src')
-            },
-            {
-                test: /\.css/,
-                loaders: ['style-loader', 'css-loader'],
-            }
-        ]
-    }
-}
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.js', '.jsx', '.json'],
+  },
+};
+
