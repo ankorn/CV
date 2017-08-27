ec59c5c42471e379dd980c96f3d0eeb2bd19d39b
Anton Korneyev <an.korn.zasekin@gmail.com>
2017-08-27 17:20:09 +0400

    made header first markup

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from '../header';
 
+import './app.css';
+
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="flex-container">
         <Header />
       </div>
     );
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import './header.css';
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <img alt="Loading..." src="https://avatars.githubusercontent.com/ankorn" />
-        <img alt="Loading..." src="https://github.com/logos" />
-        <ul>
+      <div className="header">
+        <img className="avatar" alt="Loading..." src="https://avatars.githubusercontent.com/ankorn" />
+        <ul className="navigation-bar">
           <li>Main</li>
           <li>Contacts</li>
           <li>Chat</li>
         </ul>
+        <img className="github-mark" alt="Loading..." src="src/images/GitHub-Mark-64px.png" />
       </div>
     );
   }
