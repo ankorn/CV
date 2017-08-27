1885b49a9ecbdf823742f709b76b4784f61ef074
Anton Korneyev <an.korn.zasekin@gmail.com>
2017-08-27 17:41:24 +0400

    added CVText component

---
+++ src/components/CVText/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './CVText.css';
+
+export default class CVText extends Component {
+  render() {
+    return (
+      <div className="CVText">
+        Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text.
+        Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text.
+        Text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text.
+      </div>
+    );
+  }
+}
--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../header';
+import CVText from '../CVText';
 
 import './app.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="flex-container">
+      <div>
         <Header />
+        <CVText />
       </div>
     );
   }
